15216a0b319b620e952051b944f1765d65358368
rk121 <roshankhadka613@gmail.com>
2022-06-16 20:37:55 +1000

    commit tree 798a1dd0d534fd19c3d7c1ae403e67def312acf8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+  let user = {
+    name: name,
+    email: email,
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.append(object.id);
+    })
+    .catch(function (error) {
+      document.body.append(error.message);
+    });
+}
+
+submitData("Test", "Test@email.com");
